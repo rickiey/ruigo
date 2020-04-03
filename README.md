81d4b393ab2b5417b110603e118e9d2206875b46
yangrui <yangrui@uniontech.com>
2020-04-03 14:48:14 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+### ruigo
+
++ 简易 http 框架
++ 路由正则匹配，效率低下
++ 练手项目，参考 7 天自制http框架 gee, httproute, gin
--- ruige_test.go
+++ ruige_test.go
@@ -4,9 +4,27 @@ import (
 	"fmt"
 	"regexp"
 	"testing"
+	"log"
+	"net/http"
+
+	"github.com/rickiey/ruigo"
 )
 
 func Test_re(t *testing.T) {
 	s := "GET-/he*"
 	fmt.Println(regexp.MatchString(s, "GET-/hello"))
+}
+
+func main() {
+	s := ruigo.NewServer()
+	s.Add("GET", "/hello*", Hello)
+	port := ":9000"
+	log.Println("[INFO] Start http server :", port)
+	log.Fatal(http.ListenAndServe(port, s))
+}
+
+func Hello(c *ruigo.Context) {
+	c.JSON(200, map[string]int{
+		"code": 200,
+	})
 }
